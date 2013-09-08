6a0d3e10fbac1a5ec807fd0df6e09cc85a934370
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2013-09-08 18:29:12 -0300

    Implementação da Interface Calculadora e do Stateless Bean CalculadoraBean

---
+++ calculadora/ejbModule/br/com/hro/CalculadoraBean.java
@@ -0,0 +1,31 @@
+package br.com.hro;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+
+
+@Local
+@Stateless
+public class CalculadoraBean implements Calculadora {
+
+	@Override
+	public double soma(double a, double b) {
+		return a + b;
+	}
+
+	@Override
+	public double subtracao(double a, double b) {
+		return a - b;
+	}
+
+	@Override
+	public double multiplicacao(double a, double b) {
+		return a * b;
+	}
+
+	@Override
+	public double divisao(double a, double b) {
+		return a/b;
+	}
+
+}
